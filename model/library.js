c9b7f41118eeb9377e23e12f35a2fc734b16b806
elizawura <elizabethsampong@yahoo.com>
2025-03-12 13:36:18 +0000

    multer upload added

--- model/library.js
+++ model/library.js
@@ -1,3 +1,4 @@
+// import { required } from "joi";
 import { Schema, model, modelNames } from "mongoose";
 
 const librarySchema = new Schema(
@@ -5,6 +6,7 @@ const librarySchema = new Schema(
     title: { type: String, required: true, unique: true },
     author: { type: String, required: true },
     description: { type: String, required: true },
+    image: { type: String, required: true },
     genre: { type: String },
     status: { type: String },
     publishedYear: { type: Number, required: true },
--- routes/library.js
+++ routes/library.js
@@ -1,25 +1,31 @@
 import { Router } from "express";
 // import { LibraryModel } from "../model/library.js";
 
-import { addBook , getBook, getBooks, updateEntireBook, updatePartBook, deleteBook } from "../controller/library.js"
-
-
+import {
+  addBook,
+  getBook,
+  getBooks,
+  updateEntireBook,
+  updatePartBook,
+  deleteBook,
+} from "../controller/library.js";
+import { remoteUpload } from "../middlewares/upload.js";
 
 // Create products router
 const libraryRouter = Router();
 
 //Define routes
-libraryRouter.post('/books', addBook);
+libraryRouter.post("/books", remoteUpload.single("image"), addBook);
 
-libraryRouter.get('/books', getBooks);
+libraryRouter.get("/books", getBooks);
 
-libraryRouter.get('/books/:id', getBook);
+libraryRouter.get("/books/:id", getBook);
 
-libraryRouter.patch('/books/:id', updatePartBook);
+libraryRouter.patch("/books/:id", updatePartBook);
 
-libraryRouter.put('/books/:id', updateEntireBook);
+libraryRouter.put("/books/:id", updateEntireBook);
 
-libraryRouter.delete('/books/:id', deleteBook);
+libraryRouter.delete("/books/:id", deleteBook);
 
 //Export router
 export default libraryRouter;
